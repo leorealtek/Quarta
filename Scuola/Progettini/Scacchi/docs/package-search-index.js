db5bc7acec3d79507373695551549677721416ff
leorealtek <leorealtek@gmail.com>
2026-05-28 19:10:48 +0200

    Fix

--- Scuola/Progettini/Scacchi/docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"Scuola.Progettini.Scacchi"},{"l":"Scuola.Progettini.Scacchi.Exception"},{"l":"Scuola.Progettini.Scacchi.Grafica"},{"l":"Scuola.Progettini.Scacchi.Partite"},{"l":"Scuola.Progettini.Scacchi.Pezzi"},{"l":"Scuola.Progettini.Scacchi.Util"}];updateSearchResults();
